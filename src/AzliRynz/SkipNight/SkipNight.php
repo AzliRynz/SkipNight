5f8d632b288916b95c380ea23b6c746d762764be
AzliRynz <135945845+AzliRynz@users.noreply.github.com>
2025-02-16 12:37:09 +0700

    ...

--- src/AzliRynz/SkipNight/SkipNight.php
+++ src/AzliRynz/SkipNight/SkipNight.php
@@ -5,29 +5,35 @@ declare(strict_types=1);
 namespace AzliRynz\SkipNight;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener;
-use pocketmine\event\world\TimeChangeEvent;
-use pocketmine\player\Player;
+use pocketmine\scheduler\ClosureTask;
+use pocketmine\world\World;
 
-class SkipNight extends PluginBase implements Listener {
+class SkipNight extends PluginBase {
 
     private VoteManager $voteManager;
 
     public function onEnable(): void {
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->voteManager = new VoteManager($this);
+
+        $this->getScheduler()->scheduleRepeatingTask(new ClosureTask(function (): void {
+            $this->checkNightTime();
+        }), 100);
+    }
+
+    private function checkNightTime(): void {
+        foreach ($this->getServer()->getWorldManager()->getWorlds() as $world) {
+            if ($this->isNightTime($world) && !$this->voteManager->isVoting()) {
+                $this->voteManager->startVote($world);
+            }
+        }
+    }
+
+    private function isNightTime(World $world): bool {
+        $time = $world->getTime();
+        return $time >= 12500 && $time < 23000; // Night in Minecraft
     }
 
     public function getVoteManager(): VoteManager {
         return $this->voteManager;
-    }
-
-    public function onTimeChange(TimeChangeEvent $event): void {
-        $world = $event->getWorld();
-        $time = $world->getTime();
-
-        if ($time >= 12500 && !$this->voteManager->isVoting()) {
-            $this->voteManager->startVote($world);
-        }
     }
 }
